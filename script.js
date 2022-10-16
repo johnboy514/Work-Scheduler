802af3bf252000b03de959a46405af029fcf0aa3
Swag <john.ifert.miller@gmail.com>
2022-10-16 16:29:01 -0400

    made the text box usable for local storage

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 var currentTime = moment().format("MMMM Do YYYY");
 $("#currentDay").text(currentTime);
 
-// Variables for now (as a baseline time, use moment) and HTML time block (to change color relative to now)
+// Variables for now (current hour) and HTML timeBlock (to change color for it)
 var timeBlock = $(".hour");
 var now = parseInt(moment().format("H"));
 
-// Function to check each hour block to see if it is past (gray), present (red), or future (green).
+// Function to check each hour block to see if it is past (gray), present (red), or future (green)
 $.each(timeBlock, function () {
   var hourId = parseInt($(this).attr("id"));
   if (hourId === now) {
@@ -17,3 +17,11 @@ $.each(timeBlock, function () {
     $(this).next().addClass("future");
   }
 });
+
+// Is the text box that lets you save things to local storage for events
+$(".saveBtn").on("click", function (event) {
+  var calendarItem =
+    event.target.parentElement.previousElementSibling.children[0].value;
+  localStorage.setItem(event.target.attributes[0].value, calendarItem);
+});
+
