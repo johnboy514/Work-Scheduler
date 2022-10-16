7155f473935122eff0024e6bfe3e2b8e3abcca89
Swag <john.ifert.miller@gmail.com>
2022-10-16 16:13:28 -0400

    added timestamp at top and color coordinated the times

---
+++ script.js
@@ -0,0 +1,19 @@
+// Added current day at the top of the planner (moment)
+var currentTime = moment().format("MMMM Do YYYY");
+$("#currentDay").text(currentTime);
+
+// Variables for now (as a baseline time, use moment) and HTML time block (to change color relative to now)
+var timeBlock = $(".hour");
+var now = parseInt(moment().format("H"));
+
+// Function to check each hour block to see if it is past (gray), present (red), or future (green).
+$.each(timeBlock, function (i, hour) {
+  var hourId = parseInt($(this).attr("id"));
+  if (hourId === now) {
+    $(this).next().addClass("present");
+  } else if (hourId < now) {
+    $(this).next().addClass("past");
+  } else if (hourId > now) {
+    $(this).next().addClass("future");
+  }
+});
